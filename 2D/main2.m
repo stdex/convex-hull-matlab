7ce15e956a07f89e4155e11850b5078f9e85fdc1
RostunovSergey <sergey@rostunov.com>
2015-05-02 00:44:42 +0300

    Remove pause's

--- 2D/main2.m
+++ 2D/main2.m
@@ -3,21 +3,14 @@ clear all;
 close all;
 
 X=load ('iris.txt');
-t = 0;
-
 [b, Gl_comp]=princomp(X)
 z= complex(Gl_comp(1:50,1), Gl_comp(1:50,2))
 
 [V,I]=max(abs(z-mean(z)));
-p=recu(z,z(I),t)
-t = t + 1;
-if t == 1
-    pause(5)
-end
+p=recu(z,z(I))
 krai=[p]
 for i = 1 : 8
-    pause(1)
-    p=recu(z,p,t)
+    p=recu(z,p)
     krai=[krai p]
 end
 
